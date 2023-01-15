0a20c6818aab5338e28fb9271ce404b4471d3211
Burmikk <byrmik@gmail.com>
2023-01-15 22:55:56 +0200

    task -1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+import simpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryEl = document.querySelector(".gallery")
+
+const galleryMarkup = onCreateGallaryMarkup(galleryItems)
+
+galleryEl.insertAdjacentHTML('beforeend',galleryMarkup)
+
+new SimpleLightbox('.gallery a');
+
+
+function onCreateGallaryMarkup(images) {
+    return images.map(({ preview, original, description }) => {
+       return`<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+}).join("")       
+}
