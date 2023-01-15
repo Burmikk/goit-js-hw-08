dc3fef242dd47b97062597ec278def81fb678153
Burmikk <byrmik@gmail.com>
2023-01-16 00:13:49 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from "@vimeo/player";
+import localStorageAPI from "./localStorage";
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const timeStopFromLocalStorage = localStorageAPI.load("videoplayer-current-time")
+
+checkLocalStorage()
+
+player.on('timeupdate', throttle((data) => {
+
+        localStorageAPI.save("videoplayer-current-time", data)
+ },1000));
+    
+
+
+function checkLocalStorage() {
+    if (timeStopFromLocalStorage === undefined) {
+        return
+    }
+    player.setCurrentTime(timeStopFromLocalStorage.seconds)
+
+}
+
+
+
+
+
 
